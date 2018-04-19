ae0dc5076892a0ce54b84b0e1c40c993de3a66eb
ragesh <rageshkr@asu.edu>
2018-04-18 22:20:04 -0700

    Reformated the code to match code style

--- signal_based_mapping/src/occupancy_grid/inverse_sensor_model.cpp
+++ signal_based_mapping/src/occupancy_grid/inverse_sensor_model.cpp
@@ -9,10 +9,7 @@
 
 #include "occupancy_grid/inverse_sensor_model.h"
 
-//using namespace occupancy_grid;
-
-
-double occupancy_grid::reflectance_model( double grid_range,  Stg::meters_t range, double max_range,  double noise_sd)
+double occupancy_grid::reflectance_model(double grid_range, Stg::meters_t range, double max_range, double noise_sd)
 /**
  * The function computes probability of occupancy of the grid cell corresponding to grid_range when the
  * ray was reflected.
@@ -26,15 +23,15 @@ double occupancy_grid::reflectance_model( double grid_range,  Stg::meters_t rang
   const double start_prob = 0.05;
   const double end_prob = 0.5;
   const double max_prob = 0.8;
-  const double init_slope = (end_prob - start_prob)/max_range;
-  if(grid_range <= range-2*noise_sd - 0.02){
+  const double init_slope = (end_prob - start_prob) / max_range;
+  if (grid_range <= range - 2 * noise_sd - 0.02) {
     return (init_slope * grid_range + start_prob);
-  } else{
+  } else {
     return (max_prob);
   }
 }
 
-double occupancy_grid::non_reflectance_model( double grid_range,  double max_range,  double noise_sd)
+double occupancy_grid::non_reflectance_model(double grid_range, double max_range, double noise_sd)
 /**
  * The function computes probability of occupancy of the grid cell corresponding to the grid_range when the
  * ray wasn't reflected
@@ -46,27 +43,27 @@ double occupancy_grid::non_reflectance_model( double grid_range,  double max_ran
 {
   const double start_prob = 0.05;
   const double end_prob = 0.5;
-  const double init_slope = (end_prob - start_prob)/(max_range-2*noise_sd);
-  if(grid_range <= max_range-2*noise_sd){
+  const double init_slope = (end_prob - start_prob) / (max_range - 2 * noise_sd);
+  if (grid_range <= max_range - 2 * noise_sd) {
     return (init_slope * grid_range + start_prob);
-  } else{
+  } else {
     return (end_prob);
   }
 
 }
 
-void occupancy_grid::log_odds_map_given_measurement_pose(const occupancy_grid::LaserSensor& sensor,
-                                         const int& ray_index,
-                                         std::map<double,cv::Vec<int,2>>& passed_grids_ranges,
-                                         std::list<std::pair<cv::Vec<int,2>,double>>& log_odds)
-  /**
-   * The function computes the log odds of the map cells whose coordinates are given as values in the map
-   * object passed_grids_ranges.
-   * @param sensor
-   * @param ray_index
-   * @param passed_grids_ranges
-   */
-    // TODO : explain the parameters of the function
+void occupancy_grid::log_odds_map_given_measurement_pose(const occupancy_grid::LaserSensor &sensor,
+                                                         const int &ray_index,
+                                                         std::map<double, cv::Vec<int, 2>> &passed_grids_ranges,
+                                                         std::list<std::pair<cv::Vec<int, 2>, double>> &log_odds)
+/**
+ * The function computes the log odds of the map cells whose coordinates are given as values in the map
+ * object passed_grids_ranges.
+ * @param sensor
+ * @param ray_index
+ * @param passed_grids_ranges
+ */
+// TODO : explain the parameters of the function
 {
   // find if reflectance occurred with the ray.
   // reflectance occurred if the range of the ray less than
@@ -92,44 +89,45 @@ void occupancy_grid::log_odds_map_given_measurement_pose(const occupancy_grid::L
   }
 }
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  void occupancy_grid::probability_map_given_measurement_pose(const occupancy_grid::LaserSensor& sensor, const int& ray_index,
-                                              std::map<double,cv::Vec<int,2>>& passed_grids_ranges,
-                                              std::list<std::pair<cv::Vec<int,2>,double>>& probability)
-  /**
-   * The function computes the probability of the map cells whose coordinates are given as values in the map
-   * object passed_grids_ranges.
-   * @param sensor
-   * @param ray_index
-   * @param passed_grids_ranges
-   * @param probability
-   */
-  {
+void occupancy_grid::probability_map_given_measurement_pose(const occupancy_grid::LaserSensor &sensor,
+                                                            const int &ray_index,
+                                                            std::map<double, cv::Vec<int, 2>> &passed_grids_ranges,
+                                                            std::list<std::pair<cv::Vec<int, 2>, double>> &probability)
+/**
+ * The function computes the probability of the map cells whose coordinates are given as values in the map
+ * object passed_grids_ranges.
+ * @param sensor
+ * @param ray_index
+ * @param passed_grids_ranges
+ * @param probability
+ */
+{
 
-    // find if reflectance occurred with the ray.
-    // reflectance occurred if the range of the ray less than
-    // max range - 2*sqrt(range_noise_const)
-    bool reflectance = false;
-    const double noise_variance = std::sqrt(sensor.range_noise_const);
-    if (sensor.ranges[ray_index] < (sensor.range.max - 2 * std::sqrt(noise_variance))) {
-      reflectance = true;
-    }
-
-    if (reflectance) {
-      for (auto it = passed_grids_ranges.begin(); it != passed_grids_ranges.end(); it++) {
-        double prob = reflectance_model(it->first, sensor.ranges[ray_index], sensor.range.max, noise_variance);
-        probability.push_back(std::pair<cv::Vec<int, 2>, double>(it->second, prob));
-
-      }
-    } else {
-      for (auto it = passed_grids_ranges.begin(); it != passed_grids_ranges.end(); it++) {
-        double prob = non_reflectance_model(it->first, sensor.range.max, noise_variance);
-        probability.push_back(std::pair<cv::Vec<int, 2>, double>(it->second, prob));
-      }
-
-    }
+  // find if reflectance occurred with the ray.
+  // reflectance occurred if the range of the ray less than
+  // max range - 2*sqrt(range_noise_const)
+  bool reflectance = false;
+  const double noise_variance = std::sqrt(sensor.range_noise_const);
+  if (sensor.ranges[ray_index] < (sensor.range.max - 2 * std::sqrt(noise_variance))) {
+    reflectance = true;
   }
+
+  if (reflectance) {
+    for (auto it = passed_grids_ranges.begin(); it != passed_grids_ranges.end(); it++) {
+      double prob = reflectance_model(it->first, sensor.ranges[ray_index], sensor.range.max, noise_variance);
+      probability.push_back(std::pair<cv::Vec<int, 2>, double>(it->second, prob));
+
+    }
+  } else {
+    for (auto it = passed_grids_ranges.begin(); it != passed_grids_ranges.end(); it++) {
+      double prob = non_reflectance_model(it->first, sensor.range.max, noise_variance);
+      probability.push_back(std::pair<cv::Vec<int, 2>, double>(it->second, prob));
+    }
+
+  }
+}
 
 
 
--- signal_based_mapping/src/planner/base_planner.cpp
+++ signal_based_mapping/src/planner/base_planner.cpp
@@ -6,170 +6,161 @@
 
 using namespace myPlanner;
 
+void base_planner::generate_path(double start_time) {
+  via_points point;
 
- void base_planner::generate_path(double start_time){
-    via_points point;
+  set_planStartTime(start_time);
 
-     set_planStartTime(start_time);
+  // first point in the path
+  point.modes = MOTION_MODES::ROTATION_Z;
+  point.motion_end_time = planStartTime + planTime / 2;
+  point.vel_control.a = robotTwist.a;
+  point.des_pose = startPose + Stg::Pose(0.0, 0.0, 0.0, M_PI / 4);
+  point.computed_desPose = true;
 
-    // first point in the path
-    point.modes=MOTION_MODES::ROTATION_Z;
-    point.motion_end_time=planStartTime+planTime/2;
-    point.vel_control.a=robotTwist.a;
-    point.des_pose=startPose+Stg::Pose(0.0, 0.0, 0.0, M_PI/4);
-    point.computed_desPose=true;
+  path.push(point); //  pushed the first point
 
-    path.push(point); //  pushed the first point
+  // second point in the path
+  point.modes = MOTION_MODES::TRANSLATION_X;
+  point.motion_end_time += planTime / 2;
+  point.vel_control.x = robotTwist.x;
+  point.des_pose = Stg::Pose(0.0, 0.0, 0.0, 0.0);
+  point.computed_desPose = false;
 
-    // second point in the path
-    point.modes=MOTION_MODES::TRANSLATION_X;
-    point.motion_end_time+=planTime/2;
-    point.vel_control.x=robotTwist.x;
-    point.des_pose=Stg::Pose(0.0, 0.0, 0.0, 0.0);
-    point.computed_desPose = false;
+  path.push(point);
 
-    path.push(point);
-
-     // Print the signature of the function in gcc or g++ compiler
-     //std::cout<<__PRETTY_FUNCTION__ << ": " <<std::endl ;
+  // Print the signature of the function in gcc or g++ compiler
+  //std::cout<<__PRETTY_FUNCTION__ << ": " <<std::endl ;
 
 
 
 }
 
- Stg::meters_t levyWalk_planner::generate_levy_dist() {
-    /**
-     * Generate the length from the distribution
-     */
-    //uniform random number generator
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_real_distribution<double> distribution(0.0,1.0);
-    // generate a uniform random number between 0 and 1
-    auto u = distribution(generator);
+Stg::meters_t levyWalk_planner::generate_levy_dist() {
+  /**
+   * Generate the length from the distribution
+   */
+  //uniform random number generator
+  std::random_device rd;
+  std::default_random_engine generator(rd());
+  std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  // generate a uniform random number between 0 and 1
+  auto u = distribution(generator);
+
+  if (alpha == 0) {
+    throw "\nLevy alpha should be non zero\n";
+  }
+
+  double speed = std::sqrt(get_velocity()->x * get_velocity()->x +
+      get_velocity()->y * get_velocity()->y +
+      get_velocity()->z * get_velocity()->z);
+
+  if (speed == 0) {
+    throw "\nSpeed should be non zero\n";
+  }
+
+  return levy_min * pow((1 - u), -1 / alpha); // levy walk distance
 
 
-    if(alpha == 0){
-        throw "\nLevy alpha should be non zero\n";
+}
+
+Stg::radians_t levyWalk_planner::generate_random_direction() {
+  /// Generates a random angle between min_angle and max_angle
+  //uniform random number generator
+  std::random_device rd;
+  std::default_random_engine generator(rd());
+  std::uniform_real_distribution<double> distribution(min_ang, max_ang);
+  // generate a uniform random number between min and max
+  return distribution(generator);
+}
+
+void levyWalk_planner::generate_path(double start_time) {
+  /**
+   * The function generates a path for the robot to perform levy walk in an unbounded domain
+   */
+
+  via_points point;
+
+  set_planStartTime(start_time);
+
+
+  // computing first point in the path
+  point.modes = MOTION_MODES::TRANSLATION_X;
+  auto speed = std::fabs(get_velocity()->x);
+  if (speed == 0) {
+    throw "The speed can not be zero";
+  }
+  point.motion_end_time = generate_levy_dist() / speed + start_time;
+  point.vel_control = Stg::Velocity(get_velocity()->x, get_velocity()->y, get_velocity()->z, 0);
+  point.computed_desPose = false;
+
+  path.push(point); // pushing the first point to the path
+
+
+  // computing second point in the path
+  point.modes = MOTION_MODES::ROTATION_Z;
+  auto w = std::fabs(get_velocity()->a); // omega in magnitude
+  double rotate_time = 0;
+  const Stg::radians_t &des_dir = point.des_pose.a; // ref to the desired direction
+  const Stg::radians_t &cur_dir = get_startPose()->a; // ref to the current direction
+  point.des_pose = Stg::Pose(get_startPose()->x, get_startPose()->y, get_startPose()->z, generate_random_direction());
+
+  // find the smallest angle to rotate, to find the right \omega
+  // case 1 : when both of them are ++ or --
+  if (des_dir * cur_dir >= 0) {
+    // time to complete the motion
+    rotate_time = std::fabs(des_dir - cur_dir) / std::fabs(w);
+    if (des_dir > cur_dir) {
+      // rotate in anticlockwise direction(left turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
+    } else {
+      // rotate in clockwise direction(right turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
     }
-
-
-    double speed = std::sqrt(get_velocity()->x*get_velocity()->x +
-                    get_velocity()->y*get_velocity()->y +
-                    get_velocity()->z*get_velocity()->z);
-
-
-    if(speed == 0){
-        throw "\nSpeed should be non zero\n";
+  }
+  // case 2 : when cur_dir is positive and des_dir is negative
+  if (cur_dir > 0 && des_dir < 0) {
+    // find the smallest angle between the current and the desired directions
+    if (std::fabs(cur_dir - des_dir) < std::fabs(M_PI - cur_dir) + std::fabs(-M_PI - des_dir)) {
+      // rotate in clockwise direction(right turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
+      // time to complete the motion
+      rotate_time = std::fabs(cur_dir - des_dir) / std::fabs(w);
+    } else {
+      // rotate in anticlockwise direction(left turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
+      // time to complete the motion
+      rotate_time = (std::fabs(M_PI - cur_dir) + std::fabs(-M_PI - des_dir)) / std::fabs(w);
     }
+  }
+  // case 3 : when cur_dir is negative and des_dir is positive
+  if (cur_dir < 0 && des_dir > 0) {
+    // find the smallest angle between the current and desired directions
+    if (std::fabs(cur_dir - des_dir) < std::fabs(-M_PI - cur_dir) + std::fabs(M_PI - des_dir)) {
+      // rotate in anticlockwise direction(left turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
+      // time to complete the motion
+      rotate_time = std::fabs(cur_dir - des_dir) / std::fabs(w);
+    } else {
+      // rotate in clockwise direction(right turn)
+      point.vel_control = Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
+      // time to complete the motion
+      rotate_time = (std::fabs(-M_PI - cur_dir) + std::fabs(M_PI - des_dir)) / std::fabs(w);
+    }
+  }
+  //point.motion_end_time+=rotate_time; //TODO : uncomment this after debugging
+  point.computed_desPose = true;
 
 
-    return levy_min * pow((1-u),-1/alpha); // levy walk distance
+  //path.push(point); // pushing the second point to the path //TODO : uncomment this after debugging
 
+  // computing the third point in the path
+  point.modes = MOTION_MODES::TRANSLATION_X;
+  point.motion_end_time += generate_levy_dist() / speed;
+  point.vel_control = Stg::Velocity(get_velocity()->x, get_velocity()->y, get_velocity()->z, 0);
+  point.computed_desPose = false;
 
-}
-
-
-Stg::radians_t levyWalk_planner::generate_random_direction(){
-    /// Generates a random angle between min_angle and max_angle
-    //uniform random number generator
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_real_distribution<double> distribution(min_ang, max_ang);
-    // generate a uniform random number between min and max
-    return distribution(generator);
-}
-
-
-
-
- void levyWalk_planner::generate_path(double start_time) {
-    /**
-     * The function generates a path for the robot to perform levy walk in an unbounded domain
-     */
-
-     via_points point;
-
-     set_planStartTime(start_time);
-
-
-     // computing first point in the path
-     point.modes=MOTION_MODES::TRANSLATION_X;
-     auto speed = std::fabs(get_velocity()->x);
-     if(speed == 0){
-         throw "The speed can not be zero";
-     }
-     point.motion_end_time=generate_levy_dist()/speed + start_time;
-     point.vel_control=Stg::Velocity(get_velocity()->x, get_velocity()->y, get_velocity()->z, 0);
-     point.computed_desPose= false;
-
-     path.push(point); // pushing the first point to the path
-
-
-     // computing second point in the path
-     point.modes=MOTION_MODES::ROTATION_Z;
-     auto w = std::fabs(get_velocity()->a); // omega in magnitude
-     double rotate_time = 0;
-     const Stg::radians_t& des_dir=point.des_pose.a; // ref to the desired direction
-     const Stg::radians_t& cur_dir=get_startPose()->a; // ref to the current direction
-     point.des_pose = Stg::Pose(get_startPose()->x, get_startPose()->y, get_startPose()->z, generate_random_direction());
-
-     // find the smallest angle to rotate, to find the right \omega
-     // case 1 : when both of them are ++ or --
-     if(des_dir * cur_dir >= 0){
-         // time to complete the motion
-         rotate_time=std::fabs(des_dir - cur_dir)/std::fabs(w);
-         if(des_dir > cur_dir){
-             // rotate in anticlockwise direction(left turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
-         } else {
-             // rotate in clockwise direction(right turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
-         }
-     }
-     // case 2 : when cur_dir is positive and des_dir is negative
-     if(cur_dir > 0 && des_dir < 0){
-         // find the smallest angle between the current and the desired directions
-         if (std::fabs(cur_dir - des_dir) < std::fabs(M_PI - cur_dir) + std::fabs(-M_PI - des_dir)){
-             // rotate in clockwise direction(right turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
-             // time to complete the motion
-             rotate_time=std::fabs(cur_dir - des_dir)/std::fabs(w);
-         } else {
-             // rotate in anticlockwise direction(left turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
-             // time to complete the motion
-             rotate_time=(std::fabs(M_PI - cur_dir)+std::fabs(-M_PI - des_dir))/std::fabs(w);
-         }
-     }
-     // case 3 : when cur_dir is negative and des_dir is positive
-     if(cur_dir < 0 && des_dir > 0){
-         // find the smallest angle between the current and desired directions
-         if (std::fabs(cur_dir - des_dir) < std::fabs(-M_PI - cur_dir) + std::fabs(M_PI - des_dir)){
-             // rotate in anticlockwise direction(left turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, std::fabs(w));
-             // time to complete the motion
-             rotate_time=std::fabs(cur_dir - des_dir)/std::fabs(w);
-         } else {
-             // rotate in clockwise direction(right turn)
-             point.vel_control=Stg::Velocity(0.0, 0.0, 0.0, -std::fabs(w));
-             // time to complete the motion
-             rotate_time=(std::fabs(-M_PI - cur_dir)+std::fabs(M_PI - des_dir))/std::fabs(w);
-         }
-     }
-     //point.motion_end_time+=rotate_time; //TODO : uncomment this after debugging
-     point.computed_desPose=true;
-
-
-     //path.push(point); // pushing the second point to the path //TODO : uncomment this after debugging
-
-     // computing the third point in the path
-     point.modes=MOTION_MODES::TRANSLATION_X;
-     point.motion_end_time+=generate_levy_dist()/speed;
-     point.vel_control=Stg::Velocity(get_velocity()->x, get_velocity()->y, get_velocity()->z, 0);
-     point.computed_desPose= false;
-
-     path.push(point); // pushing the third point to the path
+  path.push(point); // pushing the third point to the path
 
 
 }
