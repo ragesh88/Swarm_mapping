4a2e490f45c58112fa2a244f081fd740566fc66f
ragesh <rageshkr@asu.edu>
2018-04-18 21:22:05 -0700

    robot class cleaned from levy walk explicit variables

--- signal_based_mapping/src/robot/robot.cpp
+++ signal_based_mapping/src/robot/robot.cpp
@@ -18,42 +18,6 @@ using namespace occupancy_grid;
 int robot::gen_id=0;
 
 
- bool robot::generate_levy_dist(){
-    /// Generate the length from the distribution
-    //uniform random number generator
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_real_distribution<double> distribution(0.0,1.0);
-    // generate a uniform random number between 0 and 1
-    double u = distribution(generator);
-
-    try {
-        if(levy_alpha == 0){
-            throw "\nLevy alpha should be non zero\n";
-        }
-    }
-    catch (const char* a){
-        std::cout<<a;
-        return FAILED;
-    }
-
-    double speed = get_current_linear_speed();
-
-    try {
-        if(speed == 0){
-            throw "\nSpeed should be non zero\n";
-        }
-    }
-    catch (const char* a){
-        std::cout<<a;
-        return FAILED;
-    }
-
-    levy_dis = levy_min * pow((1-u),-1/levy_alpha); // levy flight distance
-    return SUCCESS;
-}
-
-
  Stg::radians_t robot::generate_random_direction(Stg::radians_t min, Stg::radians_t max){
     /// Generates a random angle between min and max and stores in
     /// desired_levy_direction
